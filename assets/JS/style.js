d096b8c008ccd6e1f4a03d048a90c56c12b5cdff
Manel Kimouche <79148150+ManelKimouche@users.noreply.github.com>
2021-03-19 23:54:51 +0100

    Update style.js

--- assets/JS/style.js
+++ assets/JS/style.js
@@ -1,1 +1,19 @@
 
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(elems, options);
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
